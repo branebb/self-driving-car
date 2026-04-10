77cb48d6da8021b82d2b27e33a5b4d514c09d091
branebb <branebrekalo@gmail.com>
2026-04-10 02:06:18 +0200

    car tire calculations

--- vehicle/vehicle.py
+++ vehicle/vehicle.py
@@ -7,6 +7,8 @@ class Car:
         self.velocity = velocity
         self.width = 1.9
         self.length = 3.9
+        self.tire_width = 0.29
+        self.tire_length = 0.705
         
 
     def get_corners(self):
@@ -14,13 +16,42 @@ class Car:
         L = self.length / 2
         W = self.width / 2
 
-        # local rectangle corners
+
         corners = np.array([
             [ L,  W],
             [ L, -W],
             [-L, -W],
             [-L,  W]
         ])
+
+        # rotation
+        c = np.cos(self.heading)
+        s = np.sin(self.heading)
+
+        R = np.array([
+            [c, -s],
+            [s,  c]
+        ])
+
+        rotated = (R @ corners.T).T
+        rotated[:, 0] += cx
+        rotated[:, 1] += cy
+
+        return rotated
+    
+    def get_tire_bottom_left_corners(self):
+        cx, cy = self.position
+        L = self.length / 2
+        W = self.width / 2
+
+
+        corners = np.array([
+            [ L - self.tire_length,  W - self.tire_width],
+            [ L - self.tire_length, -W ],
+            [-L , -W ],
+            [-L ,  W - self.tire_width]
+        ])
+
 
         # rotation
         c = np.cos(self.heading)
